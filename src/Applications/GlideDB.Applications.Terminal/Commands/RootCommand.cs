1d860b0e8ad70501657e9dfcc68aa502480a1731
Paul Gauci <p.gauci@outlook.com>
2024-11-29 21:13:46 +0100

    Added Terminal Application and Host library

---
+++ src/Applications/GlideDB.Applications.Terminal/Commands/RootCommand.cs
@@ -0,0 +1,14 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+
+namespace GlideDB.Applications.Terminal.Commands;
+
+[Command("GlideDB")]
+public class RootCommand
+{
+    private int OnExecute(CommandLineApplication app)
+    {
+        app.ShowHelp();
+            
+        return 0;
+    }
+}
---
+++ src/Host/GlideDB.Host/Extensions/GlideDbHostExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace GlideDB.Host.Extensions;
+
+public static class GlideDbHostExtensions
+{
+    
+    public static GlideDbHost AddSingleton<TService>(
+        this GlideDbHost glideDbHost,
+        TService implementationInstance)
+        where TService : class
+    {
+        glideDbHost.RegisterSingleton(implementationInstance);
+        
+        return glideDbHost;
+    }
+    
+    public static GlideDbHost Build(this GlideDbHost glideDbHost)
+    {
+        glideDbHost.BuildServiceProvider();
+        
+        return glideDbHost;
+    }
+    
+}
